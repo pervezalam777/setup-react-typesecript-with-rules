8537e2fc06b8bffcb8902d02156068ed14507f31
Pervez alam <pervezalam777@gmail.com>
2023-01-22 16:01:53 +0530

    first commit with es list setup

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const str = 'working';
+  console.log('value..', str);
+  const arr = [2, "4", true];
+  arr[0] = "new value";
+  console.log(arr);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
